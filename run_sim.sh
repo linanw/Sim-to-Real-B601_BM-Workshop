c3e3f4c6e71db69918e83af65d170c043b6e2210
linanw <linanwang@msn.com>
2026-06-14 21:57:40 +0800

    Add user robot resources and sim launcher

---
+++ run_sim.sh
@@ -0,0 +1,20 @@
+xhost +
+docker run --name teleop -it --privileged --gpus all -e "ACCEPT_EULA=Y" --rm --network=host \
+   -e "PRIVACY_CONSENT=Y" \
+   -e DISPLAY \
+   -v /dev:/dev \
+   -v /run/udev:/run/udev:ro \
+   -v $HOME/.Xauthority:/root/.Xauthority \
+   -v ~/docker/isaac-sim/cache/kit:/isaac-sim/kit/cache:rw \
+   -v ~/docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
+   -v ~/docker/isaac-sim/cache/pip:/root/.cache/pip:rw \
+   -v ~/docker/isaac-sim/cache/glcache:/root/.cache/nvidia/GLCache:rw \
+   -v ~/docker/isaac-sim/cache/computecache:/root/.nv/ComputeCache:rw \
+   -v ~/docker/isaac-sim/logs:/root/.nvidia-omniverse/logs:rw \
+   -v ~/docker/isaac-sim/data:/root/.local/share/ov/data:rw \
+   -v ~/docker/isaac-sim/documents:/root/Documents:rw \
+   -v ~/.cache/huggingface/lerobot/calibration:/root/.cache/huggingface/lerobot/calibration \
+   -v ~/Sim-to-Real-SO-101-Workshop/docker/env:/root/env \
+   -v ~/Sim-to-Real-SO-101-Workshop:/workspace/Sim-to-Real-SO-101-Workshop \
+   teleop-docker:latest \
+   lerobot_agent --task Lerobot-So101-Teleop-Vials-To-Rack-DR
