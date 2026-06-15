23a657e80f4e1cba15bf976771e887b71a95f220
linanw <linanwang@msn.com>
2026-06-15 14:50:36 +0800

    Export Star-Arm adapter classes

--- source/sim_to_real_so101/adapters/__init__.py
+++ source/sim_to_real_so101/adapters/__init__.py
@@ -1,2 +1,8 @@
 """Adapters that keep project-specific behavior out of vendor packages."""
 
+from .stararm102 import SimToRealStararm102Leader, SimToRealStararm102LeaderConfig
+
+__all__ = [
+    "SimToRealStararm102Leader",
+    "SimToRealStararm102LeaderConfig",
+]
